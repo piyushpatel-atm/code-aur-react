5b9ad2a8551ebbd45b858298b221f3bc95c1a01c
piyushpatel-atm <piyush29041999@gmail.com>
2024-02-15 12:43:31 +0530

    router commit

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import Home from './Components/Home/Home'
+import Header from './Components/Header/Header'
+import Footer from './Components/Footer/Footer'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+     
+    
+    </>
+  )
+}
+
+export default App
---
+++ 07reactRouter/src/Components/Github/Github.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useLoaderData } from "react-router-dom";
+
+function Github(){
+    const data = useLoaderData()
+    // const [data,setData] =useState([])
+    // useEffect(()=>{
+    //     fetch(`https://api.github.com/users/piyushpatel-atm`)
+    //     .then((response)=> response.json()).then(data => {
+    //         console.log(data)
+    //         setData(data)
+    //     })
+    // },[])
+
+    return(<>
+    
+    <div className="text-center m-4 p-4 bg-gray-600 text-white text-3xl">UserFollowers : {data.followers}
+        <img src={data.avatar_url} alt="Git picture" width={300}/>
+    </div>
+    </>);
+}
+export default Github
+
+export const githubUserInfo = async ()=>{
+   const response= await fetch(`https://api.github.com/users/piyushpatel-atm`)
+   return response.json()
+}
